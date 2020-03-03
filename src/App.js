4eb2d1764c1f80a968eb7065d8558cb0c94c4bde
xchanmolx <cmanigos@gmail.com>
2020-03-03 22:39:44 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Users />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
